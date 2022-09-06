337b01b95fd605e6015b8f98c29a5ca012f8710a
Daniel <studiosdopplermusic@gmail.com>
2022-09-06 17:01:18 -0500

    Strings

---
+++ 01_bootcam/02_strings.py
@@ -0,0 +1,12 @@
+
+# print in diferents lines
+print('1. Hello Word\n2. Hello word\n3. Hello word')
+
+#concat strings
+print ("Hello, my name is"+" "+"Daniel")
+
+#Identation Error
+    print('Error')
+
+#Syntax Error 
+print('error)
