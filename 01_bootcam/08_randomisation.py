25943a15b298b294d140d3a82c8fccd386be7c97
Daniel <studiosdopplermusic@gmail.com>
2022-09-12 14:17:05 -0500

    random module

---
+++ 01_bootcam/08_randomisation.py
@@ -0,0 +1,9 @@
+# Randomisation 
+    # Es usado en la creación de software que contenga datos aleatorios
+    
+#The PCs are deterministics, because do predecible actions
+
+import random
+
+random_integer = random.randint(1,10)
+print(random_integer)
